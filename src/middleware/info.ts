aebba4f605a658b156856655f3bd0a866c004297
Jack Baron <jackmbaron@gmail.com>
2019-05-04 03:25:17 +0100

    info middleware

--- src/middleware/info.ts
+++ src/middleware/info.ts
@@ -1,5 +1,10 @@
+import { isMaster, worker } from 'cluster'
 import { Middleware } from 'koa'
 
-export const info: Middleware = async () => {
-  // TODO: Info Middleware
+export const info: Middleware = async (ctx, next) => {
+  const workerID = isMaster ? 'M' : worker.id.toString()
+  ctx.response.set('x-worker-id', workerID)
+
+  if (process.env.HOSTNAME) ctx.response.set('x-hostname', process.env.HOSTNAME)
+  await next()
 }

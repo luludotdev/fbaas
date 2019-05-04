17ac90d7e3a3495fd3b717eb96676224040b4a85
Jack Baron <jackmbaron@gmail.com>
2019-05-04 03:31:11 +0100

    cleanup

--- src/middleware/info.ts
+++ src/middleware/info.ts
@@ -3,8 +3,8 @@ import { Middleware } from 'koa'
 
 export const info: Middleware = async (ctx, next) => {
   const workerID = isMaster ? 'M' : worker.id.toString()
-  ctx.response.set('x-worker-id', workerID)
+  ctx.set('x-worker-id', workerID)
 
-  if (process.env.HOSTNAME) ctx.response.set('x-hostname', process.env.HOSTNAME)
+  if (process.env.HOSTNAME) ctx.set('x-hostname', process.env.HOSTNAME)
   await next()
 }
